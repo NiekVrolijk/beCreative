c5f536a99b1726ad27b8245f10abe29387bc4549
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2024-06-20 12:11:38 +0200

    last script

--- beCreative/version_2/Program.cs
+++ beCreative/version_2/Program.cs
@@ -6,8 +6,45 @@
         {
             #region example2
 
+            int totalGood = 0;
+            int totalBad = 0;
+            Random random = new Random();
+
             Console.ReadLine();
 
+            while (true)
+            {
+                int good = 0;
+                int bad = 0;
+
+                for (int i = 0; i < 100; i++)
+                {
+                    int roll = random.Next(1, 21);
+                    if (roll > 10)
+                    {
+                        good++;
+                    }
+                    else
+                    {
+                        bad++;
+                    }
+                }
+
+                Console.WriteLine($"good: {good} + bad: {bad}");
+
+                if (good > 50)
+                {
+                    totalGood++;
+                }
+                else if (bad > 50)
+                {
+                    totalBad++;
+                }
+
+                Console.WriteLine($"total good: {totalGood} + total bad: {totalBad}");
+
+                Console.ReadLine();
+            }
             #endregion
         }
     }
